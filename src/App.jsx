9006f892aa35e9472dad0871ba70887a0bd8c132
Juanm0 <juanm.navarro05@gmail.com>
2026-07-11 04:02:02 -0300

    metimos cambio para ver el repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
+//comando pa githu
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
