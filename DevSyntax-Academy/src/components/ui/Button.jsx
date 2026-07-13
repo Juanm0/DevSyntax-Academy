9a250baca5ef24e389ff65b9626e7b949d683bb8
Juanm0 <juanm.navarro05@gmail.com>
2026-07-13 17:26:14 -0300

    cambiamos los cambios pero descambiamos algunas cosas con cambios

--- DevSyntax-Academy/src/components/ui/Button.jsx
+++
@@ -1,9 +0,0 @@
-import "./Button.css";
-
-export default function Button({ children, ...props }) {
-  return (
-    <button className="btn-primary" {...props}>
-      {children}
-    </button>
-  );
-}
--- DevSyntax-Academy/src/services/archivoprueba.js
+++
@@ -1,3 +0,0 @@
-import { supabase } from "../services/supabaseClient";
-
-supabase.auth.getSession().then(console.log);
